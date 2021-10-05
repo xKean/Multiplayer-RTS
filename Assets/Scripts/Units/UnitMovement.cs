cc02e1d21d12effdc3cc5fe15e7f657fa8fd0b77
Peter Braun <peterkauft@gmx.de>
2021-10-05 23:20:12 +0200

    Changed around Movement into other Script

--- Assets/Scripts/Units/UnitMovement.cs
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.InputSystem;
 
 public class UnitMovement : NetworkBehaviour
 {
@@ -16,7 +13,7 @@ public class UnitMovement : NetworkBehaviour
     #region Server
 
     [Command]
-    private void CmdMove(Vector3 position)
+    public void CmdMove(Vector3 position)
     {
         if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
 
@@ -25,31 +22,6 @@ public class UnitMovement : NetworkBehaviour
 
     #endregion
 
-    #region Client
 
-    public override void OnStartAuthority()
-    {
-        base.OnStartAuthority();
-
-        mainCamera = Camera.main;
-    }
-
-    [ClientCallback]
-    private void Update()
-    {
-        if (!hasAuthority) { return; }
-
-        if (!Mouse.current.rightButton.wasPressedThisFrame) { return; }
-
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) { return; }
-
-        CmdMove(hit.point);
-
-    }
-
-
-    #endregion
 }
 
